63bf69d555a576ae87c4ec44ea0e32cdd02b042f
popti <20112065@setu.ie>
2026-06-18 16:49:46 -0300

    add: recipie menu ui

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+package ie.setu.recipiehub.main
+
+fun main(args: Array<String>){
+
+    println("____________________________________________________________________")
+    println("|                                                                  |")
+    println("|                     Welcome to RecipieHub                        |")
+    println("|                                                                  |")
+    println("____________________________________________________________________")
+    println("|                                                                  |")
+    println("| Hi! Im your cooking app.                                         |")
+    println("|                                                                  |")
+    println("| If you'd like help with your delicous recipies..                 |")
+    println("|                                                                  |")
+    println("| Choose from the following options:                               |")
+    println("|   |-------------------------------------------------|            |")
+    println("|   |1. Add a recipie                                 |            |")
+    println("|   |2. View recipies                                 |            |")
+    println("|   |3. Delete recipies                               |            |")
+    println("|   |4. Scale your recipie for more or less quantity  |            |")
+    println("|   |                                                 |            |")
+    println("|   |-------------------------------------------------|            |")
+    println("|                                                                  |")
+    println("____________________________________________________________________")
+
+
+}
