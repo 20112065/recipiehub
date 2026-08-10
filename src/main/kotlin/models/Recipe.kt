45db6196897a37914f4452fdd07bb45f3d5ad310
popti <20112065@setu.ie>
2026-08-10 21:11:03 +0300

    Add: New model classes Recipe and Ingredients

---
+++ src/main/kotlin/models/Recipe.kt
@@ -0,0 +1,10 @@
+package ie.setu.recipiehub.main.models
+
+
+data class Recipe(val recipeTitle: String,
+                  val recipeIngredient: String,
+                  val recipeCategory: String,
+                  val recipeServingSize: Double,
+                  val ServingSize: String){
+
+}
