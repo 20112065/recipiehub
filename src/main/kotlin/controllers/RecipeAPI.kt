9ec882ef85b65661b53a9153024ab1a2d028de52
popti <20112065@setu.ie>
2026-08-20 18:28:47 +0300

    JUnit Tests for listing  active and archived recipes from the collection

--- src/main/kotlin/controllers/RecipeAPI.kt
+++ src/main/kotlin/controllers/RecipeAPI.kt
@@ -31,7 +31,44 @@ class RecipeAPI {
     }
 
     //using  a utility method to see if an index is valid in a list
-    fun isValidListIndex(index: Int, list: List<Any>): Boolean
-    { return (index >= 0) && (index < list.size)
+    fun isValidListIndex(index: Int, list: List<Any>): Boolean {
+        return (index >= 0) && (index < list.size)
+    }
+
+
+    fun listActiveRecipes(): String {
+        return if (numberOfActiveRecipes() == 0) {
+            "no active recipes stored"
+        } else {
+            var listOfActiveRecipes = ""
+            for (i in recipes.indices) {
+               if (recipes[i].isRecipeActive) {
+                   listOfActiveRecipes += "${i}: ${recipes[i]} \n"
+               }
+            }
+            listOfActiveRecipes
+        }
+    }
+
+    fun listArchivedRecipes(): String {
+        return if (numberOfArchivedRecipes() == 0) {
+            "No archived Recipes Stored"
+        } else {
+            var listOfArchivedRecipes = ""
+            for (i in recipes.indices) {
+                if (recipes[i].isRecipeArchived){
+                listOfArchivedRecipes += "${i}: ${recipes[i]} \n"
+                }
+            }
+            listOfArchivedRecipes
+        }
+    }
+
+    fun numberOfArchivedRecipes(): Int {
+        return recipes.count{ it.isRecipeArchived}
+    }
+
+    fun numberOfActiveRecipes(): Int {
+        return recipes.count{it.isRecipeActive}
     }
 }
